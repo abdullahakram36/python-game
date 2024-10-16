8841f17be956daf85fbe45c69db3084a78744244
Abdullah Akram <Abdullahakram36@gmail.com>
2024-10-16 16:22:55 +0500

    Refactored plus added graphical elements

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,28 +1,37 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
-        self.bg_color = (150, 200, 20)
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.ship = Ship(self)
         self.clock = pygame.time.Clock()
         
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-        # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)
+
+    def _check_events(self):
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-        # Make the most recently drawn screen visible.
-        self.screen.fill(self.bg_color)
+
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
         pygame.display.flip()
-        self.clock.tick(60)
 
 
 if __name__ == '__main__':
